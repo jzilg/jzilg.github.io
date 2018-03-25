7ca1eb051e19c0dbeb44ecddd0884ce3e4dbe6da
jzilg <johannes.zilg@gmail.com>
2018-03-25 09:51:38 +0200

    minify css; add watcher; remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
-const gulp      = require('gulp'),
-      prefix    = require('gulp-autoprefixer'),
-      cmq       = require('gulp-merge-media-queries'),
-      concat    = require('gulp-concat'),
-      less      = require('gulp-less'),
-      minifyCSS = require('gulp-cssnano'),
-      rename    = require('gulp-rename');
+const gulp      = require('gulp')
+const prefix    = require('gulp-autoprefixer')
+const cmq       = require('gulp-merge-media-queries')
+const less      = require('gulp-less')
+const minifyCSS = require('gulp-cssnano')
+const rename    = require('gulp-rename')
 
 /* Less to CSS
 ==============================================================================*/
 
-const srcFile = 'style/style.less';
-const cssDist = 'style/';
+const srcFile = 'style/style.less'
+const cssDist = 'style/'
 
-gulp.task('css', function() {
-    return gulp.src(srcFile)
-        .pipe(less()).on('error', console.error.bind(console))
-        .pipe(prefix({
-            browsers: [
-                'last 2 versions',
-                'last 2 Android versions',
-                'last 2 ChromeAndroid versions',
-                'last 2 FirefoxAndroid versions',
-                'last 2 iOS versions',
-                '> 5% in DE',
-                'ie >= 10'
-            ]
-        }))
-        .pipe(cmq({
-            log: false
-        }))
-        .pipe(gulp.dest(cssDist));
-});
+gulp.task('css', () => gulp.src(srcFile)
+    .pipe(less()).on('error', console.error.bind(console))
+    .pipe(prefix({
+        browsers: [
+            'last 2 versions',
+            'last 2 Android versions',
+            'last 2 ChromeAndroid versions',
+            'last 2 FirefoxAndroid versions',
+            'last 2 iOS versions',
+            '> 5% in DE',
+            'ie >= 10',
+        ]
+    }))
+    .pipe(cmq({
+        log: false
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename({
+        suffix: '.min',
+    }))
+    .pipe(gulp.dest(cssDist))
+)
+
+gulp.task('watch', () => {
+    gulp.watch(srcFile, ['css']);
+})
