a0846e169ef30a7fc367d4b6d3f268d5cc272244
jilg <johannes.zilg@gmail.com>
2019-11-01 12:00:38 +0100

    use persited or os prefered theme on start of page

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -1,20 +1,50 @@
 (function() {
     const THEME_DARK = 'dark'
     const THEME_LIGHT = 'light'
-    const THEME_LOCAL_STORAGE_ID = 'themeIsDark'
+    const THEME_STORAGE_ID = 'theme'
 
     const toggleThemeCheckbox = document.getElementById('toggle-theme-checkbox')
-    let themeIsDark = getTheme()
 
     function main() {
-        restoreTheme()
+        applyInitalTheme()
         bindToggleThemeCheckbox()
     }
 
-    function restoreTheme() {
-        if (themeIsDark) {
-            document.body.classList.add(THEME_DARK)
-            toggleThemeCheckbox.checked = true
+    function applyInitalTheme() {
+        const persistedTheme = getPersitedTheme()
+
+        if (persistedTheme) {
+            applyTheme(persistedTheme)
+            return
+        }
+
+        const darkThemeIsPrefered = matchMedia('(prefers-color-scheme: dark)').matches
+
+        if (darkThemeIsPrefered) {
+            applyTheme(THEME_DARK)
+        }
+    }
+
+    function applyTheme(theme) {
+        const themeIsDark = theme === THEME_DARK
+        const classToAdd = themeIsDark ? THEME_DARK : THEME_LIGHT
+        const classToRemove = themeIsDark ? THEME_LIGHT : THEME_DARK
+
+        document.body.classList.add(classToAdd)
+        document.body.classList.remove(classToRemove)
+
+        toggleThemeCheckbox.checked = themeIsDark
+    }
+
+    function toggleTheme() {
+        const currentThemeIsDark = document.body.classList.contains(THEME_DARK)
+
+        if (currentThemeIsDark) {
+            applyTheme(THEME_LIGHT)
+            persistTheme(THEME_LIGHT)
+        } else {
+            applyTheme(THEME_DARK)
+            persistTheme(THEME_DARK)
         }
     }
 
@@ -22,23 +52,13 @@
         toggleThemeCheckbox.addEventListener('change', toggleTheme)
     }
 
-    function toggleTheme() {
-        const classToAdd = themeIsDark ? THEME_LIGHT : THEME_DARK
-        const classToRemove = themeIsDark ? THEME_DARK : THEME_LIGHT
-        document.body.classList.add(classToAdd)
-        document.body.classList.remove(classToRemove)
-
-        themeIsDark = !themeIsDark
-        persistTheme(themeIsDark)
+    function persistTheme(theme) {
+        window.sessionStorage.setItem(THEME_STORAGE_ID, JSON.stringify(theme))
     }
 
-    function persistTheme() {
-        window.localStorage.setItem(THEME_LOCAL_STORAGE_ID, JSON.stringify(themeIsDark))
-    }
-
-    function getTheme() {
-        const persistedThemeIsDark = window.localStorage.getItem(THEME_LOCAL_STORAGE_ID)
-        return JSON.parse(persistedThemeIsDark)
+    function getPersitedTheme() {
+        const persistedTheme = window.sessionStorage.getItem(THEME_STORAGE_ID)
+        return JSON.parse(persistedTheme)
     }
 
     main()
